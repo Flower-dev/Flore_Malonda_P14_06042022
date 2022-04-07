a2de6ff1a58352fe4519bbc824b541cda0cc64ca
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2022-04-07 22:01:50 +0200

    review routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+// routes
 import { BrowserRouter } from "react-router-dom";
 import Router from './routes/routes'
 
+// ---------------------------------------------
 function App() {
   return (
     <BrowserRouter>
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 // views
 import Home from '../pages/Home';
 import EmployeeList from '../pages/EmployeeList';
@@ -6,10 +6,14 @@ import EmployeeList from '../pages/EmployeeList';
 // -------------------------------
 
 export default function Router() {
-    return (
-        <Routes>
-            <Route path= '/' element={<Home/>}/>
-            <Route path= 'employeeList' element={<EmployeeList/>}/>
-        </Routes>
-    )
+    return useRoutes([
+        {
+            path: '/',
+            element: <Home/>
+        },
+        {
+            path: 'employeeList',
+            element: <EmployeeList/>
+        }
+    ])
 }
