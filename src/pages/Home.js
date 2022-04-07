530fc193d8b98c75ff183cac3c286a288dd10964
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2022-04-07 22:56:44 +0200

    create view home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,26 @@
+import { Link } from 'react-router-dom';
+// components
+import CreateEmployee from '../components/CreateEmployee';
+
 export default function Home() {
     return (
-        <h1>BIENVENUE SUR RHNET</h1>
+        <>
+            <div classNameName='title'>
+                <h1>HRnet</h1>
+                <nav
+                    style={{
+                    borderBottom: 'solid 1px',
+                    paddingBottom: '1rem',
+                    }}
+                >
+                    <Link to='employeeList'>View Current Employees</Link> 
+                </nav>
+            </div>
+            <div classNameName='container'>
+                <h2>Create Employee</h2>
+                <CreateEmployee/>
+            </div>
+            {/* <div id='confirmation' className='modal'>Employee Created!</div> */}
+        </>
     )
 }
