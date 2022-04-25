d5b146771b56edaa03303e70efdf3a3af84c3868
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2022-04-25 22:58:02 +0200

    add pagination

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,19 +3,29 @@ import React from 'react';
 import InputForm from './FormInput';
 // custom 
 import '../custom/components/table.scss';
+// fontawesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 
 
 // ---------------------------------
-export default function TableSortBox({ tableHead, onChange, data }) {
+export default function TableSortBox({ tableHead, onChange, data, totalRows, rowsPerPage, paginate, currentPage }) {
 	const columnHeaders = [];
-  	const rowFields = [];
 	console.log(data)
 
 	// push create column headers and row fields based on tableHead object
-	for (const { header, field } of tableHead) {
-		columnHeaders.push(header);
-		rowFields.push(field);
+	for (const { header} of tableHead) {
+		columnHeaders.push(header)
 	}
+
+	//empty array of pages
+    const numberOfPages = [];
+
+    //loop over the number of pages that are needed depending on rows per page
+    for (let i = 1; i <= Math.ceil(totalRows / rowsPerPage); i++) {
+    	numberOfPages.push(i);
+    }
 
 	return (
 		<>
@@ -27,116 +37,73 @@ export default function TableSortBox({ tableHead, onChange, data }) {
 					onKeyUp={onChange}
 				/>
 			</div>
-			{data && (
-				<table>
-					<thead>
-						<tr>
-							{columnHeaders.map((header) => {
-								return (
-								<th key={header}>
-									{header}
-								</th>
-								);
-							})}
-						</tr>
-					</thead>
-					<tbody>
-						{data.map((rows) => {
-							const {
-								firstName,
-								lastName,
-								birthOfDate,
-								startOfDay,
-								state,
-								city,
-								street,
-								zipCode,
-								department,
-							} = rows;
+			
+			<table>
+				<thead>
+					<tr>
+						{columnHeaders.map((header) => {
 							return (
-								<tr className='row-data' key={rows.firstName}>
-									<td>{firstName}</td>
-									<td>{lastName}</td>
-									<td>{birthOfDate}</td>
-									<td>{startOfDay}</td>
-									<td>{state}</td>
-									<td>{city}</td>
-									<td>{street}</td>
-									<td>{zipCode}</td>
-									<td>{department}</td>
-								</tr>
+							<th align='center' key={header}>
+								{header}
+							</th>
 							);
 						})}
-					</tbody>
-				</table>
-      		)}
+					</tr>
+				</thead>
+				<tbody>
+					{data.map((rows) => {
+						return (
+							<tr className='row-data' key={rows.firstName}>
+								<td align='center'>{rows.firstName}</td>
+								<td align='center'>{rows.lastName}</td>
+								<td align='center'>{rows.dateOfBirth}</td>
+								<td align='center'>{rows.startDate}</td>
+								<td align='center'>{rows.state}</td>
+								<td align='center'>{rows.city}</td>
+								<td align='center'>{rows.street}</td>
+								<td align='center'>{rows.zipCode}</td>
+								<td align='center'>{rows.department}</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+			<div className='container'>
+				<div className='select-container'>
+					<div className='select-options'>
+					<label htmlFor='rows-per-page' className='page-label'>
+						Rows per Page:
+					</label>
+					<select
+						className='page-select'
+						name='rows'
+						id='rowNum-select'
+						onChange={onChange}>
+						<option value='5'>5</option>
+						<option value='10'>10</option>
+						<option value='20'>20</option>
+						<option value='50'>50</option>
+					</select>
+					</div>
+				</div>
+				<div className='pagination-directions'>
+					<button
+						className='direction-left'
+						onClick={() => paginate(currentPage - 1)}
+						disabled={numberOfPages.at(0) === currentPage}
+					>
+						<FontAwesomeIcon icon={faAngleLeft} className='table-icons'/>
+					</button>
+					<button className='current-center'>{currentPage}</button>
+					<button
+						className='direction-right'
+						onClick={() => paginate(currentPage + 1)}
+						disabled={numberOfPages.at(-1) === currentPage}
+					>
+						<FontAwesomeIcon icon={faAngleRight} className='table-icons'/>
+					</button>
+				</div>
+     		</div>	
 		</>
 	)
 }
-
-
-
-// import React from "react";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
-// import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
-
-// import "../pages/styles/paginationStyle.css";
-
-// const angleLeft = <FontAwesomeIcon icon={faAngleLeft} />;
-// const angleRight = <FontAwesomeIcon icon={faAngleRight} />;
-
-// const Pagination = ({
-//   totalRows,
-//   rowsPerPage,
-//   paginate,
-//   currentPage,
-//   onChange,
-// }) => {
-//   //empty array of pages
-//   const numberOfPages = [];
-
-//   //loop over the number of pages that are needed depending on rows per page
-//   for (let i = 1; i <= Math.ceil(totalRows / rowsPerPage); i++) {
-//     numberOfPages.push(i);
-//   }
-
-//   return (
-//     <div className='container'>
-//       <div className='select-container'>
-//         <p className='display-text'>
-//           Showing page <strong> {currentPage}</strong> of{" "}
-//           <strong>{numberOfPages.at(-1)}</strong>
-//         </p>
-//         <div className='select-options'>
-//           <label htmlFor='rows-per-page' className='page-label'>
-//             Rows per Page:
-//           </label>
-//           <select
-//             className='page-select'
-//             name='rows'
-//             id='rowNum-select'
-//             onChange={onChange}>
-//             <option value='10'>10</option>
-//             <option value='5'>5</option>
-//             <option value='20'>20</option>
-//             <option value='50'>50</option>
-//           </select>
-//         </div>
-//       </div>
-//       <div className='pagination-directions'>
-//         <button
-//           className='direction-left'
-//           onClick={() => paginate(currentPage - 1)}
-//           disabled={numberOfPages.at(0) === currentPage}>
-//           {angleLeft}
-//         </button>
-//         <button className='current-center'>{currentPage}</button>
-//         <button
-//           className='direction-right'
-//           onClick={() => paginate(currentPage + 1)}
-//           disabled={numberOfPages.at(-1) === currentPage}>
-//           {angleRight}
-//         </button>
-//       </div>
-//     </div>
