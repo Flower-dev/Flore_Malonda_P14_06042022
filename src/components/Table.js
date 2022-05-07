7fe7998b006aec1df650b6865e883c0a6938649c
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2022-05-07 18:47:06 +0200

    review table

--- src/components/Table.js
+++ src/components/Table.js
@@ -17,13 +17,16 @@ import {
 	TableCell,
 	TableContainer,
 	TableHead,
-	TablePagination,
 	TableRow,
 	TableSortLabel,
 	useMediaQuery
 } from '@mui/material';
+import TablePaginationUnstyled, {
+	tablePaginationUnstyledClasses as classes,
+} from '@mui/base/TablePaginationUnstyled';
 import { visuallyHidden } from '@mui/utils';
 import { useTheme } from '@mui/material/styles';
+import { styled } from '@mui/system';
 // components
 import SearchNotFound from './SearchNotFound';
 
@@ -37,6 +40,97 @@ Row.propTypes = {
 };
 
 // -------------------------------------------
+
+const blue = {
+	200: '#A5D8FF',
+	400: '#3399FF',
+};
+  
+const grey = {
+	50: '#F3F6F9',
+	100: '#E7EBF0',
+	200: '#E0E3E7',
+	300: '#CDD2D7',
+	400: '#B2BAC2',
+	500: '#A0AAB4',
+	600: '#6F7E8C',
+	700: '#3E5060',
+	800: '#2D3843',
+	900: '#1A2027',
+};
+
+
+const CustomTablePagination = styled(TablePaginationUnstyled)(
+	({ theme }) => `
+	& .${classes.spacer} {
+	  display: none;
+	}
+  
+	& .${classes.toolbar}  {
+	  margin-left: 10px;	
+	  display: flex;
+	  flex-direction: column;
+	  align-items: flex-start;
+	  gap: 10px;
+  
+	  @media (min-width: 768px) {
+		flex-direction: row;
+		align-items: center;
+	  }
+	}
+  
+	& .${classes.selectLabel} {
+	  margin: 0;
+	}
+  
+	& .${classes.select}{
+	  padding: 2px;
+	  border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[200]};
+	  border-radius: 50px;
+	  background-color: transparent;
+  
+	  &:hover {
+		background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[50]};
+	  }
+  
+	  &:focus {
+		outline: 1px solid ${theme.palette.mode === 'dark' ? blue[400] : blue[200]};
+	  }
+	}
+  
+	& .${classes.displayedRows} {
+	  margin: 0;
+	  padding: 20px;
+	  @media (min-width: 768px) {
+		margin-left: auto;
+	  }
+	}
+  
+	& .${classes.actions} {
+	  padding: 0px;
+	  border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[200]};
+	  border-radius: 50px;
+	  text-align: center;
+	}
+  
+	& .${classes.actions} > button {
+	  margin: 0 8px;
+	  border: transparent;
+	  border-radius: 2px;
+	  background-color: transparent;
+  
+	  &:hover {
+		background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[50]};
+	  }
+  
+	  &:focus {
+		outline: 1px solid ${theme.palette.mode === 'dark' ? blue[400] : blue[200]};
+	  }
+	}
+	`,
+  );
+
+  // -------------------------------------------
 
 function Row({ tableHead, tableBody, valueLabelBody, row }) {
 
@@ -260,17 +354,24 @@ export default function TableSortBox({
 				</Table>
 			</TableContainer>
 			
-
-			<TablePagination
-				labelRowsPerPage='Rows per pages:'
-				rowsPerPageOptions={[5, 10, 20, 50, { label: 'all', value: tableBody.length }]}
-				component='div'
-				count={tableBody.length}
-				rowsPerPage={rowsPerPage}
-				page={page}
-				onPageChange={handleChangePage}
-				onRowsPerPageChange={handleChangeRowsPerPage}
-			/>
+			<CustomTablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={6}
+              count={tableBody.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              componentsProps={{
+                select: {
+                  'aria-label': 'rows per page',
+                },
+                actions: {
+                  showFirstButton: true,
+                  showLastButton: true,
+                },
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
 		</Card>
 	);
 }
