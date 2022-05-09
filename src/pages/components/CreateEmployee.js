88cc60afb6f1ae3de7518145a3f9d3811594c623
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2022-05-09 13:50:10 +0200

    plugin

--- src/pages/components/CreateEmployee.js
+++ src/pages/components/CreateEmployee.js
@@ -13,6 +13,7 @@ import FormInput from '../../components/FormInput';
 import Confirmation from '../../assets/img/confirmation.png';
 // custom 
 import '../../custom/components/createEmployee.scss';
+// import DatePicker  from 'datepickerplugin';
 
 // ----------------------------------------
 
@@ -75,6 +76,9 @@ export default function CreateEmployee() {
                             <label className='labelForm'>Date of Birth</label>
                         </div>
                         <div>
+                            {/* <DatePicker 
+                                onChange={(value) => setDateOfBirth(value)}
+                            /> */}
                             <DatePicker 
                                 onChange={(value) => setDateOfBirth(value)}
                             />
